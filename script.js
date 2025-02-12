e4fe5f9e48c7958a3af1ea62f3cfd320ddc8c705
Ahmed Adel Fahmy <ahmed.hedia@outlook.com>
2025-02-12 16:23:18 +0200

    Second Change in my website

--- script.js
+++ script.js
@@ -8,4 +8,13 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         });
     });
+
+    document.querySelectorAll('.nav-link').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
 });
